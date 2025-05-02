a883db2dc6d7166dc6a5045735f74d8faf2c4873
Zouaghi M. <33163196+mouniraz@users.noreply.github.com>
2025-05-02 09:23:18 +0100

    Create MainActivity0.kt

---
+++ MainActivity0.kt
@@ -0,0 +1,71 @@
+
+package com.example.navapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navapp.ui.theme.NavAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+
+@Composable
+fun FirstCompose()
+{
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "First Compose")
+        Button(onClick = { }) {Text("next")
+        }
+    }
+
+
+}
+
+
+@Composable
+fun SecondCompose()
+{
+    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Second Compose")
+        Button(onClick = { }) {Text("next")
+       }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NavAppTheme {
+        Greeting("Android")
+    }
+}
